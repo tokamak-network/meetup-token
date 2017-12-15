2bda6ec1cf26d2c66763f10bca056ed4147f8096
Onther-inc <kemusin@naver.com>
2017-12-15 15:29:05 +0900

    remove Bulk

--- contracts/GAZUAToken.sol
+++ contracts/GAZUAToken.sol
@@ -17,13 +17,6 @@ contract GAZUAToken is Ownable, BasicNFT {
         _generateToken(beneficiary, tokenId, _metadata);
     }
 
-    function generateTokenBulk(address[] beneficiary, uint[] tokenId, string _metadata) public onlyOwner {
-        require(beneficiary.length == tokenId.length);
-        for (uint i = 0; i < tokenId.length; i++) {
-            generateToken(beneficiary[i], tokenId[i], _metadata);
-        }
-    }
-
     function _generateToken(address beneficiary, uint tokenId, string _metadata) internal {
         _addTokenTo(beneficiary, tokenId);
         totalTokens++;
@@ -32,14 +25,14 @@ contract GAZUAToken is Ownable, BasicNFT {
         Created(tokenId, beneficiary, _metadata);
     }
 
-// no one can update metadata
+    // no one can update metadata
     function updateTokenMetadata(uint tokenId, string _metadata) public {
          throw; 
     }
 
-    function setLimitation(uint _quantity) public onlyOwner returns (bool) {
+    function addLimitation(uint _quantity) public onlyOwner returns (bool) {
         limitation = limitation.add(_quantity);
         return true;
     }
-
+    
 }
